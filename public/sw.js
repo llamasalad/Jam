686bf2243afba3c7a38917d34a3ccb329006fcfc
llamasalad <llamasalad2@gmail.com>
2026-04-16 19:31:28 +0300

    Hotfix 15

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jam-v1';
+const CACHE = 'jam-v2';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
