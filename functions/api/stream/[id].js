16c9542dd9bc16d25598a07fd57ea21c6df186f2
llamasalad <llamasalad2@gmail.com>
2026-04-26 14:32:41 +0300

    v2 Nightly

--- functions/api/stream/[id].js
+++ functions/api/stream/[id].js
@@ -52,17 +52,5 @@ export async function onRequestGet({ params, request, env }) {
 
   headers["Content-Length"] = String(object.size);
 
-  const cache = caches.default;
-  const cacheKey = new Request(request.url);
-  const cachedRes = await cache.match(cacheKey);
-  if (cachedRes) return cachedRes;
-
-  const response = new Response(object.body, { status: 200, headers });
-
-  // only cache full requests, not range requests
-  if (!rangeHeader) {
-    await cache.put(cacheKey, response.clone());
-  }
-
-  return response;
+  return new Response(object.body, { status: 200, headers });
 }
