18ef19bcc668fb92c30b950e9799080d55b2bd97
llamasalad <llamasalad2@gmail.com>
2026-05-28 00:49:43 +0300

    v.1.280

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jam-v279';
+const CACHE = 'jam-v280';
 const ASSETS = [
   '/', '/index.html', '/style.css', '/app.js', '/manifest.json',
   '/api/font/subset-SFProDisplay-Regular.woff2',
